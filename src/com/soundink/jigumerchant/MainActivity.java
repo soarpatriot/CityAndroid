2c48fed4c926cb90162c5ec4e0efda87ab2900b0
soar <85624529@qq.com>
2014-11-26 15:35:06 +0800

    first push for the simple app

---
+++ src/com/soundink/jigumerchant/MainActivity.java
@@ -0,0 +1,50 @@
+package com.soundink.jigumerchant;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity{
+      
+	private WebView webView;  
+	@Override  
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		webView = (WebView) findViewById(R.id.merchant_staff);  
+        
+        webView.getSettings().setJavaScriptEnabled(true);//设置使用够执行JS脚本  
+        webView.getSettings().setBuiltInZoomControls(false);
+
+        webView.loadUrl("http://inkash.test.soundink.net/");  
+        webView.setWebViewClient(new WebViewClient(){  
+            @Override  
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {  
+                // TODO Auto-generated method stub  
+                view.loadUrl(url);// 使用当前WebView处理跳转  
+                return true;//true表示此事件在此处被处理，不需要再广播  
+            }  
+            @Override   //转向错误时的处理  
+            public void onReceivedError(WebView view, int errorCode,  
+                    String description, String failingUrl) {  
+                // TODO Auto-generated method stub  
+                Toast.makeText(MainActivity.this, R.string.network_error, Toast.LENGTH_SHORT).show();  
+            }  
+        });  
+		
+	}
+    
+	@Override   //默认点回退键，会退出Activity，需监听按键操作，使回退在WebView内发生  
+    public boolean onKeyDown(int keyCode, KeyEvent event) {  
+        // TODO Auto-generated method stub  
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {  
+            webView.goBack();  
+            return true;  
+        }  
+        return super.onKeyDown(keyCode, event);  
+    }  
+
+}
